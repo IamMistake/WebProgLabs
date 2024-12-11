63a06d391998f704a5ac131ac1d4115e598b7268
IamMistake <jagurinoskini@gmail.com>
2024-12-11 09:18:09 +0100

    initial commite

---
+++ src/main/java/mk/ukim/finki/wp/laba/LabsGroupAApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.laba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsGroupAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsGroupAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mk/ukim/finki/wp/laba/LabsGroupAApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp.laba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabsGroupAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
